9666725092ebc1d4c3dae98e505a1270c80b8d20
Camilo Soto Montoya <csotomon@hotmail.com>
2020-06-16 15:45:51 -0500

    varios

--- visualizacion/covid/models.py
+++ visualizacion/covid/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import User, Group
 from django.db import models
 
-# Create your models here.
+class Poi(models.Model):
+    name = models.CharField(max_length=50)
+    latitude = models.CharField(max_length=10)
+    longitude = models.CharField(max_length=10)
+
+class Tracking(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateTimeField()
+    
--- visualizacion/covid/views.py
+++ visualizacion/covid/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from .serializers import UserSerializer, GroupSerializer, TrackingSerializer, PoiSerializer
+from .models import Tracking, Poi
 
-# Create your views here.
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+
+class TrackingViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Tracking.objects.all()
+    serializer_class = TrackingSerializer
+
+
+class PoiViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Poi.objects.all()
+    serializer_class = PoiSerializer
